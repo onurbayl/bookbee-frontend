e59fd251132a1f9bf56f055939d40b3ac72b4d3f
grnonur <greenpikachuu@gmail.com>
2024-12-15 15:05:01 +0300

    deprecated ReactDOM issue resolved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css'; // Global styles
 import App from './App'; // Main App component
 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 // Render the App component into the root div in index.html
-ReactDOM.render(
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root') // Matches the root div in public/index.html
+ // Matches the root div in public/index.html
 );
