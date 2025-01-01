fe64ac494d974794b491cb8ac4fcbeca680efc13
Burak-Toprak <brk_toprak_1903@hotmail.com>
2025-01-01 23:21:15 +0300

    BookGrid pagination fix

--- src/pages/components/BookGrid/BookGrid.jsx
+++ src/pages/components/BookGrid/BookGrid.jsx
@@ -95,9 +95,9 @@ const BookGrid = ({ books, enableEdit = false, enableSort = true }) => {
                     </Link>
                 ))}
             </div>
-            <div className="pagination">
+            <div className="bookgrid-pagination">
                 <button
-                    className="pagination-button"
+                    className="bookgrid-pagination-button"
                     onClick={() => handlePageChange(currentPage - 1)}
                     disabled={currentPage === 1}
                 >
@@ -106,14 +106,14 @@ const BookGrid = ({ books, enableEdit = false, enableSort = true }) => {
                 {Array.from({ length: totalPages }, (_, index) => (
                     <button
                         key={index + 1}
-                        className={`pagination-button ${currentPage === index + 1 ? "active" : ""}`}
+                        className={`bookgrid-pagination-button ${currentPage === index + 1 ? "active" : ""}`}
                         onClick={() => handlePageChange(index + 1)}
                     >
                         {index + 1}
                     </button>
                 ))}
                 <button
-                    className="pagination-button"
+                    className="bookgrid-pagination-button"
                     onClick={() => handlePageChange(currentPage + 1)}
                     disabled={currentPage === totalPages}
                 >
